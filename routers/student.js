700bc0e6490518e51a2d21f260eaf211f2f61765
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-29 00:20:31 +0530

    Update student.js

---
+++ routers/student.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json([
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe" },
+    { id: 3, name: "John Smith" },
+  ]);
+});
