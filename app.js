f3194ef45ce9294c3cafc4d1f484036218721330
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-29 00:16:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const student = require("./routes/student");
+const user = require("./routes/user");
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/student", student);
+app.use("/api/user", user);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
