69c9bda8674de5d66bfccd0d060c673b51626f60
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-29 14:08:58 +0530

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const router = express.Router();
 
 router.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
-  const token = jwt.sign(user, "hello my name is token");
+  const token = jwt.sign(user, process.env.EN_KEY);
   res.send({ token });
 
   //sample hhtp request url
