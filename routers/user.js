b768d5a9fdc7cd004ab1dca13187c47cc0a2d16b
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-29 13:14:59 +0530

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -3,14 +3,8 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/login", (req, res) => {
-  // dan mrhtna ape username ekai password ekai ewanwa
-  // eka check karnwa database eke
-  // ok nam
-
   const username = req.body.username;
-  // payload
   const user = { name: username };
-
   const token = jwt.sign(user, "hello my name is token");
   res.send({ token });
 });
