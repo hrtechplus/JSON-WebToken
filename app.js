0ab6eb7935639dd6ad04b1bfeac1078fb31054a6
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-29 18:00:33 +0530

    apps,student

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const student = require("./routers/student");
 const user = require("./routers/user");
 const port = process.env.PORT || 3000;
+const auth = require("./middleware/auth");
 
 app.use(express.json());
-//app.use("/api/student", student);
-app.use("/api/user", user);
+app.use("/api/student", student);
+app.use("/api/user", auth, user);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
--- routers/student.js
+++ routers/student.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json([
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Doe" },
-    { id: 3, name: "John Smith" },
-  ]);
+  const user = req.user;
+  res.send(user);
+  // res.json([
+  //   { id: 1, name: "John Doe" },
+  //   { id: 2, name: "Jane Doe" },
+  //   { id: 3, name: "John Smith" },
+  // ]);
 });
 
 exports.router = router;
